74564aba54b2fa1648d5ca9378ebfcec195a20ca
Thet Tun <thettun1741997@gmail.com>
2020-03-14 00:38:59 +0630

    change to github

---
+++ README.md
@@ -0,0 +1,2 @@
+## This is Mycareer Project 
+## In this project, We use Laravel 6, Bootstrap 4
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/template.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    @stack('css')
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="custom-nav-bar">
+                <div class="nav-logo">
+                    <a href="http://" class="monster-logo">
+                        <img src="{{asset('images/monster-logo.svg')}}" alt="">
+                    </a>
+                </div>
+                <div class="nav-wapper">
+                    <ul class="custom-nav-items">
+                        <li class="nav-link"><a href="http://" class="nav-link-name">Job Search</a></li>
+                        <li class="nav-link"><a href="http://" class="nav-link-name">Fresher</a></li>
+                        <li class="nav-link"><a href="http://" class="nav-link-name">Blog</a></li>
+                        <li class="nav-link"><a href="http://" class="nav-link-name">About</a></li>
+                    </ul>
+                    <ul class="login-section-nav">
+                        <li class="nav-link">
+                            <a href="" class="cbtn nav-link-name"><i class="fa fa-user" aria-hidden="true"></i>Login</a></li>
+                            <li class="nav-link"><a href="" class="cbtn nav-link-name"><i class="fa fa-users" aria-hidden="true"></i>Employer Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="jumbotron jumbotron-fluid bg-jumbotron">
+            <div class="container">
+                <div class="search-engin">
+                    <form class="user-search-form">
+                        <div class="form-row">
+                            <div class="col-md-4">
+                                <input type="text" class="form-control custom-input" placeholder="Search by Skills, Company & Job Title">
+                            </div>
+                            <div class="input-group mb-2 mr-sm-2 col-md-3">
+                                <div class="input-group-prepend ">
+                                    <div class="input-group-text custom-input">@</div>
+                                </div>
+                                <input type="text" class="form-control custom-input" id="location" placeholder="Location">
+                            </div>
+                            <div class="col-md-3">
+                                <select class="form-control custom-input">
+                                    <option>Default select</option>
+                                </select>
+                            </div>
+                            <div class="col-md-2">
+                                <input type="submit" value="Search" class="custom-btn ">
+                            </div>
+                        </div>
+                    </form>
+                    <a href="http://" class="search-model d-flex justify-content-end">Advanced Search</a>
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
