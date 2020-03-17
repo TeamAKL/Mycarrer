e25004a9a4f9b55e4b7b727a2ac49d8ed4cf52de
HlwanNaing <hlwannaing114@gmail.com>
2020-03-17 23:00:52 +0630

    for dropdown

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,10 +23,30 @@
                 </div>
                 <div class="nav-wapper">
                     <ul class="custom-nav-items">
-                        <li class="nav-link"><a href="http://" class="nav-link-name">Job Search</a></li>
-                        <li class="nav-link"><a href="http://" class="nav-link-name">Fresher</a></li>
-                        <li class="nav-link"><a href="http://" class="nav-link-name">Blog</a></li>
-                        <li class="nav-link"><a href="http://" class="nav-link-name">About</a></li>
+                       <li class="dropdown">
+                          <a href="" class="dropbtn">Dropdown</a>
+                          <div class="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                          </div>
+                        </li>
+                        <li class="dropdown">
+                          <a href="" class="dropbtn">Dropdown</a>
+                          <div class="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                          </div>
+                        </li>
+                        <li class="dropdown">
+                          <a href="" class="dropbtn">Dropdown</a>
+                          <div class="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                          </div>
+                        </li>
                     </ul>
                     <ul class="login-section-nav">
                         <li class="nav-link">
