d1a550441ef60169dcc3e97951c2b89ac563e1a9
Thet Tun <thettun1741997@gmail.com>
2020-04-09 10:24:54 +0630

    finished adminpanel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,30 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
     /**
-    * Create a new controller instance.
-    *
-    * @return void
-    */
-    public function __construct()
-    {
-        // $this->middleware('auth');
-    }
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
-    * Show the application dashboard.
-    *
-    * @return \Illuminate\Contracts\Support\Renderable
-    */
-    public function index(Request $request)
-    {
-        $ip = $request->ip();
-        $request->session()->put('ip_address', $ip);
-        $val = $request->session()->get('ip_address');
-        return view('home', ['count' => collect($val)->count()]);
-    }
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\View\View
+     */
+    // public function index()
+    // {
+    //     return view('dashboard');
+    // }
 }
