03347f5911ee1be0c02bf5445ccc38162dd85255
yumon <hlwannaing116@gmail.com>
2020-07-20 23:17:12 +0630

    for sliders

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,50 +67,7 @@
 
     <!-- For Service -->
 
-    <div id="fh5co-staff">
-		<div class="container">
-            <div class="col-md-12">
-                <h3 class="heading-title text-center">
-                    SERVICES <span>We are looking forward to being your partner.</span>
-                </h3>
-            </div>
-			<div class="row">
-				<div class="col-md-4 col-sm-6 col-xs-12 text-center">
-					<div class="staff">
-						<div class="staff-img" style="background-image: url({{asset('images/it.jpg')}});">	
-						</div>
-						
-						<h3><a href="#">Mike Smith</a></h3>
-						<p>Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit.
-                           Nullam ac urna eu felis dapibus condimentum. Sed ut imperdiet nisi.</p>
-                        <a href="" class="btn btn-second btn-full">Detail</a>
-                    </div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12 text-center">
-					<div class="staff">
-						<div class="staff-img" style="background-image: url({{asset('images/it.jpg')}});">	
-						</div>
-						
-						<h3><a href="#">Mike Smith</a></h3>
-						<p>Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. 
-                           Nullam ac urna eu felis dapibus condimentum.Sed ut imperdiet nisi.</p>
-                        <a href="" class="btn btn-second btn-full">Detail</a>
-                    </div>
-				</div>
-				<div class="col-md-4 col-sm-6  col-xs-12 text-center">
-					<div class="staff">
-						<div class="staff-img" style="background-image: url({{asset('images/it.jpg')}});">
-						</div>
-						
-						<h3><a href="#">Mike Smith</a></h3>
-						<p>Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. 
-                           Nullam ac urna eu felis dapibus condimentum. Sed ut imperdiet nisi.</p>
-                        <a href="" class="btn btn-second btn-full">Detail</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 
     @endsection
 
